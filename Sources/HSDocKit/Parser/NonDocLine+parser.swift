e049c7ef798534d3c6448ab1ea67d1917485a68d
David Peterson <david@randombits.org>
2022-02-09 12:04:56 +1000

    Moved `NonDocLine` to its own file.

---
+++ Sources/HSDocKit/Parser/NonDocLine+parser.swift
@@ -0,0 +1,34 @@
+import Parsing
+
+struct NonDocLine: Parser {
+    func parse(_ input: inout TextDocument) throws -> Void {
+        guard let firstLine = input.first else {
+            throw ParsingError.expectedInput("at least one line", at: input)
+        }
+        
+        var text = firstLine.text
+        
+        try Not { docPrefix }.parse(&text)
+        
+        input = input.dropFirst()
+        return ()
+    }
+}
+
+struct NonDocLines: Parser {
+    func parse(_ input: inout TextDocument) -> UInt {
+        let nonDocLine = NonDocLine()
+        var count: UInt = 0
+        
+        while !input.isEmpty {
+            do {
+                try nonDocLine.parse(&input)
+                count = count + 1
+            } catch {
+                break
+            }
+        }
+        
+        return count
+    }
+}
